21a36706421bb806fbbd8613fd6dfc232c18f547
franklin rodriguez <franklinserif@gmail.com>
2025-02-01 18:47:09 -0400

    feat(home_screen.dart): :sparkles: add header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/media.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      // we want scroll effect
+      body: ListView(
+        children: [
+          Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Good Morning', style: AppStyles.headLineStyle3),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text('Book Tickets', style: AppStyles.headLineStyle1),
+                        ],
+                      ),
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: const DecorationImage(
+                                image: AssetImage(AppMedia.logo))),
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text('Search Icon'), Text('Empty space')],
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
   }
 }
