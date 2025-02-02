01784ba5cca17538ebd0f4a5cd832bee84244605
franklin rodriguez <franklinserif@gmail.com>
2025-02-01 21:51:41 -0400

    feat(app_styles): :sparkles: add new color bgColor = 0xFFeeedf12

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -5,6 +5,7 @@ Color primary = const Color(0xFF687daf);
 class AppStyles {
   static Color primaryColor = primary;
   static Color textColor = const Color(0xFF3b3b3b);
+  static Color bgColor = const Color(0xFFeeedf2);
 
   static TextStyle headLineStyle1 = TextStyle(
     fontSize: 26,
