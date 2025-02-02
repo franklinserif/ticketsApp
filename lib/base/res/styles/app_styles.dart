5dffc96aa4d8da698e2e7e781456d29499ead1b4
franklin rodriguez <franklinserif@gmail.com>
2025-02-01 21:53:17 -0400

    feat(app_styles): :sparkles: add testStyle

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -7,6 +7,9 @@ class AppStyles {
   static Color textColor = const Color(0xFF3b3b3b);
   static Color bgColor = const Color(0xFFeeedf2);
 
+  static TextStyle textStyle =
+      TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.w500);
+
   static TextStyle headLineStyle1 = TextStyle(
     fontSize: 26,
     fontWeight: FontWeight.w700,
