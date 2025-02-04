6825abb0f6517c56c7d9944166f74172c6ff4ec5
franklin rodriguez <franklinserif@gmail.com>
2025-02-03 20:22:33 -0400

    feat(app_styles): :sparkles: add new variables and styles

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -7,6 +7,7 @@ class AppStyles {
   static Color textColor = const Color(0xFF3b3b3b);
   static Color bgColor = const Color(0xFFeeedf2);
   static Color ticketBlue = const Color(0xFF526799);
+  static Color ticketOrage = const Color(0xFFF37B67);
 
   static TextStyle textStyle =
       TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.w500);
@@ -25,6 +26,11 @@ class AppStyles {
 
   static TextStyle headLineStyle3 = const TextStyle(
     fontSize: 17,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle headLineStyle4 = const TextStyle(
+    fontSize: 15,
     fontWeight: FontWeight.w500,
   );
 }
