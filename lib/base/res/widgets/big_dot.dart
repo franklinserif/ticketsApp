c65200aeed3decac63628be645d9bde7a26802a5
franklin rodriguez <franklinserif@gmail.com>
2025-02-03 20:23:30 -0400

    refactor(big_dot): :recycle: add real implementation

--- lib/base/res/widgets/big_dot.dart
+++ lib/base/res/widgets/big_dot.dart
@@ -5,6 +5,12 @@ class BigDot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: EdgeInsets.all(3),
+      decoration: BoxDecoration(
+        border: Border.all(width: 2.5, color: Colors.white),
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
   }
 }
