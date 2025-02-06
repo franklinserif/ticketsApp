f0f31a27be5654bd8841343b69fba755aabdfee3
franklin rodriguez <franklinserif@gmail.com>
2025-02-05 20:14:22 -0400

    feat(widgets): :sparkles: add hotel

---
+++ lib/screens/widgets/hotel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Hotel extends StatelessWidget {
+  const Hotel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
