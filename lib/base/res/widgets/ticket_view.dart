c9d43edb8bfef357b75d0fe0028ace82f956efc2
franklin rodriguez <franklinserif@gmail.com>
2025-02-05 20:18:03 -0400

    refactor(ticket_view): :recycle: add dynamic values to ticket_view

--- lib/base/res/widgets/ticket_view.dart
+++ lib/base/res/widgets/ticket_view.dart
@@ -8,7 +8,10 @@ import 'package:ticket_app/base/res/widgets/text_style_fourth.dart';
 import 'package:ticket_app/base/res/widgets/text_style_third.dart';
 
 class TicketView extends StatelessWidget {
-  const TicketView({super.key});
+  final Map<String, dynamic> ticket;
+  final bool wholeScreen;
+
+  const TicketView({super.key, required this.ticket, this.wholeScreen = false});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +21,7 @@ class TicketView extends StatelessWidget {
       width: size.width * 0.85,
       height: 189,
       child: Container(
-        margin: const EdgeInsets.only(right: 16),
+        margin: EdgeInsets.only(right: wholeScreen == true ? 0 : 16),
         child: Column(
           children: [
             // blue part of the tickets
@@ -34,7 +37,7 @@ class TicketView extends StatelessWidget {
                   // Show departure and destination with icons first line
                   Row(
                     children: [
-                      const TextStyleThird(text: 'NYC'),
+                      TextStyleThird(text: ticket['from']['code']),
                       Expanded(child: Container()),
                       const BigDot(),
                       Expanded(
@@ -56,24 +59,24 @@ class TicketView extends StatelessWidget {
                       )),
                       const BigDot(),
                       Expanded(child: Container()),
-                      const TextStyleThird(text: 'LDN')
+                      TextStyleThird(text: ticket['to']['code'])
                     ],
                   ),
                   const SizedBox(height: 3),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const SizedBox(
+                      SizedBox(
                         width: 100,
-                        child: TextStyleFourth(text: 'New-York'),
+                        child: TextStyleFourth(text: ticket['from']['name']),
                       ),
                       Expanded(child: Container()),
-                      const TextStyleFourth(text: '8H 30M'),
+                      TextStyleFourth(text: ticket['flying_time']),
                       Expanded(child: Container()),
-                      const SizedBox(
+                      SizedBox(
                         width: 100,
                         child: TextStyleFourth(
-                          text: 'London',
+                          text: ticket['to']['name'],
                           align: TextAlign.end,
                         ),
                       ),
@@ -124,24 +127,24 @@ class TicketView extends StatelessWidget {
                   borderRadius: const BorderRadius.only(
                       bottomLeft: Radius.circular(21),
                       bottomRight: Radius.circular(21))),
-              child: const Column(
+              child: Column(
                 children: [
                   // Show departure and destination with icons first line
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       AppColoumTextLayout(
-                        topText: '1 May',
+                        topText: ticket['date'],
                         bottomText: 'Date',
                         crossAxis: CrossAxisAlignment.start,
                       ),
                       AppColoumTextLayout(
-                        topText: '08:00 AM',
+                        topText: ticket['departure_time'],
                         bottomText: 'Departure time',
                         crossAxis: CrossAxisAlignment.center,
                       ),
                       AppColoumTextLayout(
-                        topText: '23',
+                        topText: ticket['number'].toString(),
                         bottomText: 'Number',
                         crossAxis: CrossAxisAlignment.end,
                       ),
